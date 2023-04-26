0ec8ef6a793c47b07870fa49aa18ed8a5e5911d3
kalkidan12 <kalget12@gmail.com>
2023-04-26 12:49:41 +0300

    theming the app

---
+++ lib/theme/theme_config.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ThemeConfig {
+  static Color lightPrimary = Colors.white;
+  static Color darkPrimary = Color(0xff1f1f1f);
+  static Color lightAccent = Color.fromARGB(255, 128, 24, 202);
+  static Color darkAccent =
+      Color.fromARGB(255, 128, 24, 202); // Color(0xff2ca8e2);
+  static Color lightBG = Colors.white;
+  static Color darkBG = Color(0xff121212);
+
+  static ThemeData lightTheme = ThemeData(
+    backgroundColor: lightBG,
+    primaryColor: lightPrimary,
+    scaffoldBackgroundColor: lightBG,
+    appBarTheme: AppBarTheme(
+      color: lightPrimary,
+      elevation: 0.0,
+      titleTextStyle: TextStyle(
+        color: Colors.black,
+        fontSize: 20,
+        fontWeight: FontWeight.w800,
+      ),
+      iconTheme: IconThemeData(
+        color: Colors.black,
+      ),
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    backgroundColor: darkBG,
+    primaryColor: darkPrimary,
+    scaffoldBackgroundColor: darkBG,
+    appBarTheme: AppBarTheme(
+      color: darkPrimary,
+      elevation: 0.0,
+      titleTextStyle: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+        fontWeight: FontWeight.w800,
+      ),
+      iconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+    ),
+  );
+}
